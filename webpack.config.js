3ca31aef296a6436a2e090dc67440f98b6587ea0
povilasurbonas <povilas.urbonas85@gmail.com>
2020-03-21 01:05:56 +0100

    Choosing a Development Tool

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      // inject: false,
-      // template: require('html-webpack-template'),
       hash: true,
       title: 'Test',
       myPageHeader: 'Hello World',
