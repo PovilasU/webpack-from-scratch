a25c68bac2e1f6f671e7a368e2b616ba80177b1d
povilasurbonas <povilas.urbonas85@gmail.com>
2020-03-21 14:25:37 +0100

    git reset

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 
 ReactDOM.render(
-  <Welcome name="component. Works fine.test"></Welcome>,
+  <Welcome name="component. Works fine.test.."></Welcome>,
   document.getElementById('root')
 );
