8282432d54939949541e4c376bf7426a069c006b
povilasurbonas <povilas.urbonas85@gmail.com>
2020-03-21 19:19:47 +0100

    added brotli plugin for webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin;
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-
+const BrotliPlugin = require('brotli-webpack-plugin');
 module.exports = merge(common, {
   mode: 'production',
   // devtool: 'source-map',
@@ -25,6 +25,12 @@ module.exports = merge(common, {
       filename: '[path].gz[query]',
       algorithm: 'gzip',
       test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }),
+    new BrotliPlugin({
+      asset: '[path].br[query]',
+      test: /\.(js|css|html|svg)$/,
       threshold: 10240,
       minRatio: 0.8
     }),
