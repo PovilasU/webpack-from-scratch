212cdb49e7ec1694a032a5687e6f94db726927ed
povilasurbonas <povilas.urbonas85@gmail.com>
2020-03-24 07:36:36 +0100

    seervice worker test

--- webpack.prod.js
+++ webpack.prod.js
@@ -41,7 +41,7 @@ module.exports = merge(common, {
     new CleanWebpackPlugin(),
     new CopyPlugin([
       { from: 'src/robots.txt', to: 'robots.txt' },
-      { from: 'src/serviceWorker.js', to: 'service-worker.js' },
+      { from: 'src/service-worker.js', to: 'service-worker.js' },
       { from: 'src/manifest.json', to: 'manifest.json' }
     ])
   ],
