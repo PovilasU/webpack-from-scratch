7770fddcf404ad10f7b2cd702cda99d67ec18dda
povilasurbonas <povilas.urbonas85@gmail.com>
2020-04-14 21:39:38 +0200

    installed Webpack plugin to remove unused css.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const glob = require('glob');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -13,55 +14,39 @@ const CompressionPlugin = require('compression-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const { GenerateSW } = require('workbox-webpack-plugin');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+};
 
 module.exports = merge(common, {
   mode: 'production',
   // devtool: 'source-map',
   output: {
     filename: '[name].[contentHash].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
-
-  plugins: [
-    //  new BundleAnalyzerPlugin(),
-    // new WorkboxWebpackPlugin.InjectManifest({
-    //   swSrc: './src/serviceWorker.js',
-    //   swDest: 'sw.js'
-    // }),
-    new GenerateSW(),
-    // new InjectManifest({
-    //   swSrc: './src/serviceWorker.js'
-    // }),
-
-    new CompressionPlugin({
-      filename: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8
-    }),
-
-    new OptimizeCssAssetsPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contentHash].css'
-    }),
-    new CleanWebpackPlugin(),
-    new CopyPlugin([
-      { from: 'src/robots.txt', to: 'robots.txt' }
-      // { from: 'src/service-worker.js', to: 'service-worker.js' }
-      // { from: 'src/manifest.json', to: 'manifest.json' }
-    ])
-  ],
 
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
     minimize: true,
     minimizer: [
       new TerserPlugin(),
@@ -78,9 +63,43 @@ module.exports = merge(common, {
           removeRedundantAttributes: true,
           removeScriptTypeAttributes: true,
           removeStyleLinkTypeAttributes: true,
-          useShortDoctype: true
-        }
-      })
-    ]
-  }
+          useShortDoctype: true,
+        },
+      }),
+    ],
+  },
+  plugins: [
+    //  new BundleAnalyzerPlugin(),
+    // new WorkboxWebpackPlugin.InjectManifest({
+    //   swSrc: './src/serviceWorker.js',
+    //   swDest: 'sw.js'
+    // }),
+    new GenerateSW(),
+    // new InjectManifest({
+    //   swSrc: './src/serviceWorker.js'
+    // }),
+
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+
+    new OptimizeCssAssetsPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contentHash].css',
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    }),
+
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: 'src/robots.txt', to: 'robots.txt' },
+      // { from: 'src/service-worker.js', to: 'service-worker.js' }
+      // { from: 'src/manifest.json', to: 'manifest.json' }
+    ]),
+  ],
 });
